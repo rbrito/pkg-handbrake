e91655a22759c8439ccca663d5700b4860cf2b82
Rogério Brito <rbrito@ime.usp.br>
2012-04-23 07:30:26 -0300

    Minor fix for compilation.

--- libhb/decdca.c
+++ libhb/decdca.c
@@ -258,7 +258,7 @@ static hb_buffer_t * Decode( hb_work_object_t * w )
 
     for( i = 0; i < num_blocks; i++ )
     {
-        dca_sample_t * samples_in;
+        sample_t * samples_in;
         float    * samples_out;
 
         dca_block( pv->state );
